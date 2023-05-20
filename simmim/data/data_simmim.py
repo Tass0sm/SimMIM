4df830ce189996a8bcbfee68204f4f81c5da3e01
Tassos Manganaris <tassos.manganaris@gmail.com>
2023-05-20 02:56:50 -0400

    Remove transformations for monai compat. Add spacing for formatter

--- simmim/data/data_simmim.py
+++ simmim/data/data_simmim.py
@@ -24,36 +24,36 @@ class MaskGenerator:
         self.mask_patch_size = mask_patch_size
         self.model_patch_size = model_patch_size
         self.mask_ratio = mask_ratio
-        
+
         assert self.input_size % self.mask_patch_size == 0
         assert self.mask_patch_size % self.model_patch_size == 0
-        
+
         self.rand_size = self.input_size // self.mask_patch_size
         self.scale = self.mask_patch_size // self.model_patch_size
-        
+
         self.token_count = self.rand_size ** 2
         self.mask_count = int(np.ceil(self.token_count * self.mask_ratio))
-        
+
     def __call__(self):
         mask_idx = np.random.permutation(self.token_count)[:self.mask_count]
         mask = np.zeros(self.token_count, dtype=int)
         mask[mask_idx] = 1
-        
+
         mask = mask.reshape((self.rand_size, self.rand_size))
         mask = mask.repeat(self.scale, axis=0).repeat(self.scale, axis=1)
-        
+
         return mask
 
 
 class SimMIMTransform:
     def __init__(self, config):
-        self.transform_img = T.Compose([
-            T.Lambda(lambda img: img.convert('RGB') if img.mode != 'RGB' else img),
-            T.RandomResizedCrop(config.DATA.IMG_SIZE, scale=(0.67, 1.), ratio=(3. / 4., 4. / 3.)),
-            T.RandomHorizontalFlip(),
-            T.ToTensor(),
-            T.Normalize(mean=torch.tensor(IMAGENET_DEFAULT_MEAN),std=torch.tensor(IMAGENET_DEFAULT_STD)),
-        ])
+        # self.transform_img = T.Compose([
+        #     # T.Lambda(lambda img: img.convert('RGB') if img.mode != 'RGB' else img),
+        #     # T.RandomResizedCrop(config.DATA.IMG_SIZE, scale=(0.67, 1.), ratio=(3. / 4., 4. / 3.)),
+        #     # T.RandomHorizontalFlip(),
+        #     # T.ToTensor(),
+        #     # T.Normalize(mean=torch.tensor(IMAGENET_DEFAULT_MEAN),std=torch.tensor(IMAGENET_DEFAULT_STD)),
+        # ])
  
         if config.MODEL.TYPE == 'swin':
             model_patch_size=config.MODEL.SWIN.PATCH_SIZE
@@ -61,18 +61,18 @@ class SimMIMTransform:
             model_patch_size=config.MODEL.VIT.PATCH_SIZE
         else:
             raise NotImplementedError
-        
+
         self.mask_generator = MaskGenerator(
             input_size=config.DATA.IMG_SIZE,
             mask_patch_size=config.DATA.MASK_PATCH_SIZE,
             model_patch_size=model_patch_size,
             mask_ratio=config.DATA.MASK_RATIO,
         )
-    
+
     def __call__(self, img):
-        img = self.transform_img(img)
+        # img = self.transform_img(img)
         mask = self.mask_generator()
-        
+
         return img, mask
 
 
